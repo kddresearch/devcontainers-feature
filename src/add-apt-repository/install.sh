b3ac843e340576011944622588a1a773d73901c6
Legonois <74224353+Legonois@users.noreply.github.com>
2024-10-24 05:16:05 +0000

    Fixed update bug

--- src/add-apt-repository/install.sh
+++ src/add-apt-repository/install.sh
@@ -1,8 +1,8 @@
 # TODO: Add a check to see if the package is already installed
 # TODO: Add a check to see if apt-get is installed
 
-sudo apt update
-sudo apt install software-properties-common
+sudo apt update -y
+sudo apt install software-properties-common -y
 
 # if sudo apt-get --simulate install packageThatDoesntExist      
 # then echo "we're good"
