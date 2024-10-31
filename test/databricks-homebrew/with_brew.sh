3ae8a1b1206c2445d4e4b46ab4ff51ce2f041ce5
Legonois <74224353+Legonois@users.noreply.github.com>
2024-10-30 21:05:25 -0500

    Added tests

--- test/databricks-homebrew/with_brew.sh
+++ test/databricks-homebrew/with_brew.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "hugo --help"
+check "execute command" bash -c "databricks --version"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
