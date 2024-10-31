f7164dbf9e8fbbef2699d3c705680352ea87414f
Legonois <74224353+Legonois@users.noreply.github.com>
2024-10-30 20:43:28 -0500

    Began simple json file

--- src/databricks-homebrew/install.sh
+++ src/databricks-homebrew/install.sh
@@ -9,14 +9,15 @@ source ./library_scripts.sh
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.4.39"
+ensure_nanolayer nanolayer_location "v0.5.6"
 
 
 $nanolayer_location \
     install \
     devcontainer-feature \
     "ghcr.io/devcontainers-contrib/features/homebrew-package:1" \
-    --option package='hugo' \
+    --option tap='databricks/tap' \
+    --option package='databricks' \
     --option version="$VERSION"
 
 echo 'Done!'
